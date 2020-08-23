0cbb2b4a9db7093d585d7b1c6128db1d06f245f6
Robert Powell <r.powelliv@gmail.com>
2020-08-23 11:17:31 -0700

    Scene

---
+++ index.js
@@ -0,0 +1,19 @@
+// console.log(movieData);
+
+// $(document).ready( ()=> {
+//     function renderMovies(movieArray) {
+//         movieHTML = movieArray.map(movie => {
+//             // console.log(movie);
+//             console.log(movie.Title);
+//             console.log(movie.Year);
+//             $('#image').html(movie.Poster)
+//         });
+//     };
+//     renderMovies(movieData);
+//     // $('#search').click((renderMovies) => {
+    
+//     // console.log(movieHTML.join(''));
+//     console.log('Testing...');
+// });
+
+
