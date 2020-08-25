cb9845dcd6fd400e9092a171632983b703eec35d
Robert <r.powelliv@gmail.com>
2020-08-25 13:23:46 -0700

    newcomp

--- index.js
+++ index.js
@@ -13,11 +13,29 @@ $(document).ready( ()=> {
               <p class="card-text" id="release-date">${movie.Year}</p>
               <a href="#" class="btn btn-primary">ADD</a>
             </div>`
-        } )
+        });
 
         $('.results').html(movieHtml.join(' '))
     }
-
-    renderMovie(movieData)
+    renderMovie(movieData);
 
 });
+
+    function getMovie() {
+        var movieFind = document.getElementById("search").value;
+
+        console.log(movieFind);
+    
+        movieData.forEach(function(item, index){
+          if(item.movieFind == movieFind) {
+
+            renderMovie(movieData);
+            // var displayData = "<li><b>User Name</b>: "+ item.username +"</li>"+
+            //       "<li><b>EMail</b>: "+ item.email +"</li>"+
+            //       "<li><b>Status</b>: "+ item.status +"</li>"+
+            //       "<li><b>ID</b>: "+ item.id +"</li>";     
+            // $("#display").html(displayData);
+          };
+        });
+    };
+
