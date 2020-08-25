1c324b6597f382d949dcac7618f05a9a07847ee1
Robert <r.powelliv@gmail.com>
2020-08-25 10:26:30 -0700

    newcomp

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ $(document).ready( ()=> {
         // console.log(movieArray)
 
         movieHtml = movieArray.map( movie => {
-            return `<p>${movie.Title}</p>`
+            return `<div class="card" style="width: 18rem;">
+            <img class="card-img-top"id="image" src="${movie.Poster}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title" id="title">${movie.Title}</h5>
+              <p class="card-text" id="release-date">${movie.Year}</p>
+              <a href="#" class="btn btn-primary">ADD</a>
+            </div>`
         } )
 
         $('.results').html(movieHtml.join(' '))
