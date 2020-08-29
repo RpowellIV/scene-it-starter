de69a2e7a2c44488f55875ebc193afff96c07abc
Robert <r.powelliv@gmail.com>
2020-08-29 08:08:43 -0700

    RobP

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-
-// $(function(){
-
-// 	function renderMovies(movieArray) {
-
-// 		var finalHTML = "";
-
-// 		movieArray.forEach(function(currentMovie){
-
-// 			finalHTML += '<div class="card" style="width: 20rem;">';
-// 			finalHTML += '<img class="card-img-top" src="'+ currentMovie.Poster +'"/>';
-// 			finalHTML += '<div class="card-body">';
-// 			finalHTML += '<h4 class="card-title">'+ currentMovie.Title;
-// 			finalHTML += '<span class="badge badge-secondary float-right">'+ currentMovie.Year +'</span>';
-// 			finalHTML += '</h4>';
-// 			finalHTML += '<a href="#" class="btn btn-primary addButton">Add!</a>';
-// 			finalHTML += '</div>';
-// 			finalHTML += '</div>';
-// 		});
-
-// 		return finalHTML;
-// 	}
-// })
-
-
 $(()=> {
     console.log("Ready")
 
@@ -59,17 +34,7 @@ $(()=> {
         console.log(newMovie);
      
       
-      // movieHTML = renderMovies(movieData)
-      // let movieFind = $(".search-bar").val();
-      // console.log(`This is ${movieFind}`);   
-      // movieData.forEach(function(item, index){
-      //   if(item.movieFind === movieFind) {
-      //     console.log(item.movieFind);
-      //     var choice = renderMovies(movieData);
-      //     console.log(choice);
-      //   };
-    
-      // });
+
       });
 
 
@@ -98,3 +63,14 @@ $(()=> {
 };
 
 
+      // movieHTML = renderMovies(movieData)
+      // let movieFind = $(".search-bar").val();
+      // console.log(`This is ${movieFind}`);   
+      // movieData.forEach(function(item, index){
+      //   if(item.movieFind === movieFind) {
+      //     console.log(item.movieFind);
+      //     var choice = renderMovies(movieData);
+      //     console.log(choice);
+      //   };
+    
+      // });
